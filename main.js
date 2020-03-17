b61cf79d9ce65a381341fd9d6d472b5272058b93
ViolettaNespolo <violetta.nespolo@journalism.cuny.edu>
2020-03-17 18:42:12 -0400

    edits

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 $("body").on("keyup keydown keypress change", ".item input", function (e) {
-	let total = 0;
+	let pre_tariff_total_sum = 0;
+	let post_tariff_total_sum = 0;
 
 	$("tr.item").each(function () {
 		const $this_row = $(this);
@@ -9,22 +10,39 @@ $("body").on("keyup keydown keypress change", ".item input", function (e) {
 		let cost = cost_field.val();
 		const tariff_pct = $this_row.data("tariff-percent");
 		let pct = parseFloat(tariff_pct);
-
-		console.log("tariff", tariff_pct);
-		console.log("pct", pct);
-
-
 		pct = pct / 100;
 
 		//make them into numbers because otherwise it reads input as a string, parseFloat works with decimals
 		qty = parseFloat(qty);
 		cost = parseFloat(cost);
 
+
 		const pre_tariff_total = qty * cost;
 
 		const post_tariff_total = (pre_tariff_total + (pre_tariff_total * pct));
 
+		pre_tariff_total_sum = pre_tariff_total_sum + pre_tariff_total;
+		post_tariff_total_sum = post_tariff_total_sum + post_tariff_total;
+
+		console.log("pre tariff total");
 		console.log(pre_tariff_total);
+
+		$this_row.find("td.pre_total span").text(pre_tariff_total);
+
+		$("#pre_total span").text(pre_tariff_total_sum);
+
+		console.log("post tariff total");
 		console.log(post_tariff_total);
-	});
+
+		$this_row.find("td.post_total span").text(post_tariff_total);
+
+		$("#post_total span").text(post_tariff_total_sum);
+
+	}); //this ends the each loop
+
+	console.log("pre tariff total sum");
+	console.log(pre_tariff_total_sum);
+
+	console.log("post tariff total sum");
+	console.log(post_tariff_total_sum);
 });
